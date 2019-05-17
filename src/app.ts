998bd13e871ad22b3d3ce848f8263048e25af346
VJ <vishnuvijay@factor5.com.au>
2019-05-18 01:38:06 +1000

    Searching  for info completed
    columnify package was also added to get greater control over the console display

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import { getAvailableSearchFields } from './data';
-var path = require('path');
-
-const readlineSync = require('readline-sync');
+import { getAvailableSearchFields, getAllInfo } from './data';
+import process from 'process';
+import path from 'path';
+import readlineSync from 'readline-sync';
+import columnify from 'columnify';
 
 const primaryOptions = ['Search Zendesk', 'View a list of searchable fields'];
 const searchOptions = ['Users', 'Tickets', 'Organizations'];
@@ -10,7 +11,7 @@ console.log('\nSelect search options:');
 const index = readlineSync.keyInSelect(primaryOptions, 'Please select your option', {
   cancel: 'Quit',
 });
-console.log('\nYou have selected to ' + primaryOptions[index]);
+index >= 0 ? console.log('\nYou have selected to ' + primaryOptions[index]) : process.exit();
 
 switch (index) {
   case 0:
@@ -20,22 +21,47 @@ switch (index) {
     viewSearchField();
     break;
   default:
-    quitOperation();
+    defaultOperation();
 }
 
-function searchZendesk() {
+async function searchZendesk() {
   console.log('\nPlease select on which the search should be based on:');
   const index = readlineSync.keyInSelect(searchOptions, 'Please select your option', {
     cancel: 'Quit',
   });
   console.log('\nYou have selected ' + searchOptions[index]);
-  console.log('\nPlease enter search term:');
+  const searchTerm = readlineSync.question('Please enter the search term: ');
+  let searchableFields: any = [];
+  await getAvailableSearchFields(
+    path.join(__dirname, '../asset/json/' + searchOptions[index].toLowerCase() + '.json')
+  ).then(result => {
+    searchableFields = result;
+
+    let searchValue = '';
+    let info: any = [];
+
+    searchableFields.includes(searchTerm.trim())
+      ? ((searchValue = readlineSync.question('Please enter the search value: ')),
+        getAllInfo(
+          searchTerm,
+          searchValue,
+          path.join(__dirname, '../asset/json/' + searchOptions[index].toLowerCase() + '.json')
+        ).then(result => {
+          info = result;
+          info.map((item: any) => {
+            console.log(
+              '\n' + columnify(item, { columns: ['Terms', 'Values'], columnSplitter: ' : ' })
+            );
+          });
+        }))
+      : console.log('\n The field value entered is not valid search field');
+  });
 }
 
 function viewSearchField() {
   let searchableFields: any = [];
   searchOptions.forEach(async item => {
-    const value = await getAvailableSearchFields(
+    await getAvailableSearchFields(
       path.join(__dirname, '../asset/json/' + item.toLowerCase() + '.json')
     ).then(result => {
       searchableFields = result;
@@ -46,10 +72,9 @@ function viewSearchField() {
         console.log(item);
       });
     }
-    // console.log(value);
   });
 }
 
-function quitOperation() {
-  console.log('\nQuit');
+function defaultOperation() {
+  console.log('\n Please select an appropriate operation');
 }
--- src/data.ts
+++ src/data.ts
@@ -1,21 +1,45 @@
 import { pipeline } from 'stream';
+import fs from 'fs';
+import { parser } from 'stream-json';
+import Asm from 'stream-json/Assembler';
 
-const { parser } = require('stream-json');
-const fs = require('fs');
-const Asm = require('stream-json/Assembler');
-
-export function getAvailableSearchFields(path: string) {
+const getAssembler = (path: string) => {
   const pipeline = fs.createReadStream(path).pipe(parser());
   const asm = Asm.connectTo(pipeline);
+  return asm;
+};
+
+export const getAvailableSearchFields = (path: string) => {
+  const asm = getAssembler(path);
   return new Promise((resolve, reject) => {
     asm.on('done', (asm: any) => {
-      const value = Object.keys(asm.current[0]);
+      const value: Array<string> = Object.keys(asm.current[0]);
       if (value) {
         resolve(value);
       }
     });
   }).then(value => {
-    // console.log(value);
     return value;
   });
-}
+};
+
+export const getAllInfo = (searchTerm: string, searchValue: string, path: string) => {
+  const asm = getAssembler(path);
+  return new Promise((resolve, reject) => {
+    asm.on('done', (asm: any) => {
+      const info: Array<string> = asm.current.filter(
+        (item: any) =>
+          item[searchTerm] &&
+          item[searchTerm]
+            .toString()
+            .toUpperCase()
+            .trim() === searchValue.toUpperCase().trim()
+      );
+      if (info) {
+        resolve(info);
+      }
+    });
+  }).then(info => {
+    return info;
+  });
+};
